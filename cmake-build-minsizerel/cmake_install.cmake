a428a88627a4605804db2ec10a1b57f1bb361a76
Rammy Aly <ramaly544@gmail.com>
2023-11-16 09:49:34 -0600

    clean up cloned folders

--- cmake-build-minsizerel/cmake_install.cmake
+++ cmake-build-minsizerel/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Owner/projects/posh
+# Install script for directory: C:/Users/Owner/projects/posh/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Owner/projects/posh/cmake-build-minsizerel/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Owner/projects/posh/src/cmake-build-minsizerel/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
